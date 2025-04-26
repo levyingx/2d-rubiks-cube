b166374fe1f7d272c81ebe87cd6f1501b4b02d8e
Levy <levyfernandes14@gmail.com>
2025-04-26 03:28:34 -0300

    Add code to objects

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import './style.css'
 
-const red = {name: 'red', array: Array(9).fill('red')};
-const green = {name: 'green', array: Array(9).fill('green')};
-const blue = {name: 'blue', array: Array(9).fill('blue')};
-const white = {name: 'white', array: Array(9).fill('white')};
-const yellow = {name: 'yellow', array: Array(9).fill('yellow')};
-const orange = {name: 'orange', array: Array(9).fill('orange')};
+const red = {code: 'R', name: 'red', array: Array(9).fill('red')};
+const green = {code: 'F', name: 'green', array: Array(9).fill('green')};
+const blue = {code: 'B', name: 'blue', array: Array(9).fill('blue')};
+const white = {code: 'U', name: 'white', array: Array(9).fill('white')};
+const yellow = {code: 'D', name: 'yellow', array: Array(9).fill('yellow')};
+const orange = {code: 'L', name: 'orange', array: Array(9).fill('orange')};
 
 function render(object) {
   const element = document.createElement('div');
@@ -22,7 +22,19 @@ function render(object) {
 }
 
 function R() {
-  
+  // do R according to F perspective
+  // green[2, 5, 8] go to white[2, 5, 8]
+  // white[2, 5, 8] go to blue[2, 5, 8]
+  // blue [2, 5, 8] go to yellow[2, 5, 8]
+  // yellow[2, 5, 8] go to blue[2, 5, 8]
+
+  // generalizing:
+  // F[2, 5, 8] go to U[2, 5, 8]
+
+  // U[2, 5, 8] go to B[2, 5, 8]
+  // B[2, 5, 8] go to D[2, 5, 8]
+  // D[2, 5, 8] go to B[2, 5, 8]
+
 }
 
 render(orange);
